e723f7c57ebff82d76fd0fd089798c1026158c61
Martín Calderón <martincalderon92@outlook.com>
2022-11-07 23:33:38 -0300

    Create Navbar component

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Container, List, ListItem, Underline } from "./styles";
+
+const items = ["About Me", "Projects", "Skills", "Contact"];
+
+const Navbar = () => {
+  const [activeItem, setActiveItem] = useState<string>(items[0]);
+
+  const handleItemClick = (item: string) => {
+    setActiveItem(item);
+  };
+
+  return (
+    <Container>
+      <List>
+        {items?.map((item) => (
+          <ListItem
+            key={item}
+            onClick={() => handleItemClick(item)}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            {item}
+
+            {activeItem === item && (
+              <Underline
+                animate={{ width: "100%" }}
+                initial={{ width: "0%" }}
+                transition={{ duration: 0.5, type: "spring", stiffness: 120 }}
+              />
+            )}
+          </ListItem>
+        ))}
+      </List>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ components/Navbar/styles.tsx
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0.5rem 2rem;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const List = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 1rem;
+`;
+
+export const ListItem = styled(motion.li)`
+  margin-right: 1rem;
+  cursor: pointer;
+`;
+
+export const Underline = styled(motion.div)`
+  height: 3px;
+  background-color: #000;
+  bottom: -5px;
+  left: 0;
+  right: 0;
+`;
