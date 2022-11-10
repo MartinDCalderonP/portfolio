f6c74858cab06a156fb1a24f551c3a5151e29d1d
Martín Calderón <martincalderon92@outlook.com>
2022-11-10 19:03:15 -0300

    Change background color

--- components/Navbar/styles.tsx
+++ components/Navbar/styles.tsx
@@ -6,7 +6,6 @@ export const Container = styled.div`
   justify-content: flex-end;
   align-items: center;
   padding: 0.5rem 2rem;
-  background-color: #fff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
