b241fcc8dde670eef2acab54035fb45c0db5e8fb
Martín Calderón <martincalderon92@outlook.com>
2022-11-10 23:13:15 -0300

    Add Projects component

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,5 +1,106 @@
+import Image from "next/image";
+import { FiGithub, FiLink } from "react-icons/fi";
+import Card from "../Card";
+import BoardGames from "../../images/board-games.png";
+import GoogleClone from "../../images/google-clone.png";
+import YouTubeClone from "../../images/youtube-clone.png";
+import IMDBApp from "../../images/imdb-app.png";
+import {
+  Container,
+  Description,
+  LinkButton,
+  LinksContainer,
+  Picture,
+  ProjectCard,
+  Tags,
+} from "./styles";
+
+const projects = [
+  {
+    title: "IMDB App",
+    description:
+      "A IMDB app that allows you to search for movies and tv shows. It uses the OMDB API to fetch the data.",
+    image: IMDBApp,
+    tags: [
+      "React",
+      "TypeScript",
+      "Custom Hooks",
+      "Context API",
+      "ESLint",
+      "SASS Modules",
+      "Jest",
+      "Mock Service Worker",
+    ],
+    repoUrl: "https://github.com/MartinDCalderonP/imdb-app-final-project",
+    deployUrl: "https://imdb-app-final-project.vercel.app/",
+  },
+  {
+    title: "YouTube Clone",
+    description:
+      "A YouTube clone that allows you to search for videos. It uses the YouTube API to fetch the data.",
+    image: YouTubeClone,
+    tags: ["React", "Material UI", "Custom Hooks", "Context API", "Firebase"],
+    repoUrl: "https://github.com/MartinDCalderonP/YouTube-Clone",
+    deployUrl: "https://clone-6c8d2.web.app/",
+  },
+  {
+    title: "Google Clone",
+    description:
+      "A Google clone that allows you to search for anything. It uses the Google API to fetch the data.",
+    image: GoogleClone,
+    tags: ["React", "SASS Modules", "Custom Hooks", "Context API", "Firebase"],
+    repoUrl: "https://github.com/MartinDCalderonP/Google-Clone",
+    deployUrl: "https://clone-d3b31.web.app/",
+  },
+  {
+    title: "Board Games",
+    description:
+      "A Board Games app where you can play Tic Tac Toe and a Memory game",
+    image: BoardGames,
+    tags: ["React", "SASS", "Custom Hooks", "TypeScript"],
+    repoUrl: "https://github.com/MartinDCalderonP/board-games-week-07",
+    deployUrl: "https://board-games-week-07.vercel.app/",
+  },
+];
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <Card title="Projects">
+      <Container>
+        {projects.map((project) => (
+          <ProjectCard key={project.title}>
+            <p>{project.title}</p>
+
+            <Picture>
+              <Image alt={project.title} src={project.image} />
+            </Picture>
+
+            <Description>{project.description}</Description>
+
+            <Tags>{project.tags.join(", ")}</Tags>
+
+            <LinksContainer>
+              <LinkButton
+                href={project.deployUrl}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <FiLink />
+              </LinkButton>
+
+              <LinkButton
+                href={project.repoUrl}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <FiGithub />
+              </LinkButton>
+            </LinksContainer>
+          </ProjectCard>
+        ))}
+      </Container>
+    </Card>
+  );
 };
 
 export default Projects;
---
+++ components/Projects/styles.tsx
@@ -0,0 +1,79 @@
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
+
+export const Container = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  flex-wrap: wrap;
+  gap: 2rem;
+  margin-top: 2rem;
+`;
+
+export const ProjectCard = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 500px;
+  height: 500px;
+  padding: 1rem;
+  border-radius: 10px;
+  border: 1px solid #00539cff;
+  padding-top: 1.25rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  background-color: #9cc3d5ff;
+`;
+
+export const Picture = styled(motion.picture)`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const Description = styled(motion.p)`
+  font-size: 1.25rem;
+  font-weight: 500;
+  color: #00539cff;
+  margin: 1rem 0;
+  text-align: center;
+`;
+
+export const Tags = styled(motion.p)`
+  font-size: 0.9rem;
+  font-weight: 300;
+  color: #00539cff;
+  margin-top: 1rem;
+  width: 75%;
+`;
+
+export const LinksContainer = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 1rem;
+`;
+
+export const LinkButton = styled(motion.a)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 10px;
+  border: 1px solid #00539cff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  background-color: #9cc3d5ff;
+  cursor: pointer;
+  text-decoration: none;
+`;
