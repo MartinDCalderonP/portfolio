dd0a9b4c855737ffcce0af4268e0a0af2ddf0081
Martín Calderón <martincalderon92@outlook.com>
2022-11-10 21:35:48 -0300

    Fix a text in AboutMe component

--- components/AboutMe/index.tsx
+++ components/AboutMe/index.tsx
@@ -3,7 +3,7 @@ import { Container, TitleUnderline } from "./styles";
 
 const texts = [
   "I'm a Front End Developer with a passion for building web applications and learning new technologies. 📖",
-  "I've been working with React for the past 2 years. I've also worked with Next, Gatsby, Styled Components, Tailwind and other technologies. 👨🏽‍💻",
+  "I've been working with React since September 2021. I've also worked with Next, Gatsby, Styled Components, Tailwind and other technologies. 👨🏽‍💻",
   "My hobbies include reading, playing with my two cats, cooking and watching movies. 🎞",
   "I know Spanish, English, Portuguese and a little bit of French. 🌎",
   "I was born in Buenos Aires and I love my city. 🇦🇷",
