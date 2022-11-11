d723fc703ee55df6ab66b5f2fd3f95115c26a7fb
Martín Calderón <martincalderon92@outlook.com>
2022-11-10 21:18:04 -0300

    Add About Me component

--- components/AboutMe/index.tsx
+++ components/AboutMe/index.tsx
@@ -1,5 +1,26 @@
+import Card from "../Card";
+import { Container, TitleUnderline } from "./styles";
+
+const texts = [
+  "I'm a Front End Developer with a passion for building web applications and learning new technologies. 📖",
+  "I've been working with React for the past 2 years. I've also worked with Next, Gatsby, Styled Components, Tailwind and other technologies. 👨🏽‍💻",
+  "My hobbies include reading, playing with my two cats, cooking and watching movies. 🎞",
+  "I know Spanish, English, Portuguese and a little bit of French. 🌎",
+  "I was born in Buenos Aires and I love my city. 🇦🇷",
+];
+
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  return (
+    <Card>
+      <Container>
+        <h1>About Me</h1>
+        <TitleUnderline />
+        {texts?.map((text, index) => (
+          <p key={index}>{text}</p>
+        ))}
+      </Container>
+    </Card>
+  );
 };
 
 export default AboutMe;
---
+++ components/AboutMe/styles.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 1rem;
+  padding: 1rem;
+  width: 100%;
+  height: 100%;
+`;
+
+export const TitleUnderline = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: #00539cff;
+`;
