bc9859bc6e281316652bedf9caf952d3f5c7dae7
Martín Calderón <martincalderon92@outlook.com>
2022-11-28 14:35:13 -0300

    Add new text to About Me

--- components/AboutMe/index.tsx
+++ components/AboutMe/index.tsx
@@ -4,7 +4,8 @@ import { Container } from "./styles";
 
 const texts = [
   "I'm a Front End Developer with a passion for building web applications and learning new technologies. 📖",
-  "I've been working with React since September 2021. I've also worked with Next, Gatsby, Styled Components, Tailwind and other technologies. 👨🏽‍💻",
+  "I've working experience with React since September 2021 and I'm looking for a new challenge. 🚀",
+  "I've also worked with Next, Gatsby, Emotion, Styled Components, Tailwind and other technologies. 👨🏽‍💻",
   "My hobbies include reading, playing with my two cats, cooking and watching movies. 🎞",
   "I know Spanish, English, Portuguese and a little bit of French. 🌎",
   "I was born in Buenos Aires and I love my city. 🇦🇷",
