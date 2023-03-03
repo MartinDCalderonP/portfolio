ebd3417f3bb4b30622e784c245e6b35e94c65ff9
Martín Calderón <martincalderon92@outlook.com>
2023-03-03 18:58:27 -0300

    Add to calculate the years of experience of each technology

--- components/TechStack/index.tsx
+++ components/TechStack/index.tsx
@@ -1,6 +1,8 @@
 import Card from "components/Card";
 import { Container, Column, Row, TechCard, LogoImage } from "./styles";
 
+const today = new Date();
+
 const tech = [
   {
     title: "Frontend",
@@ -8,61 +10,92 @@ const tech = [
       {
         name: "HTML 5",
         image: "https://img.icons8.com/color/48/000000/html-5--v1.png",
-        years: 3,
+        years: today.getFullYear() - 2019,
       },
       {
         name: "CSS 3",
         image: "https://img.icons8.com/color/48/000000/css3.png",
-        years: 3,
+        years: today.getFullYear() - 2019,
       },
       {
         name: "SASS",
         image: "https://img.icons8.com/color/48/000000/sass.png",
-        years: 1,
+        years: today.getFullYear() - 2020,
       },
       {
         name: "JavaScript",
         image: "https://img.icons8.com/color/48/000000/javascript--v1.png",
-        years: 3,
+        years: today.getFullYear() - 2019,
       },
       {
         name: "TypeScript",
         image: "https://img.icons8.com/color/48/000000/typescript.png",
-        years: 1,
+        years: today.getFullYear() - 2021,
       },
       {
         name: "React",
         image: "https://img.icons8.com/color/48/000000/react-native.png",
-        years: 2,
+        years: today.getFullYear() - 2020,
+      },
+      {
+        name: "React Native",
+        image: "https://img.icons8.com/color/48/000000/react-native.png",
+        years: today.getFullYear() - 2021,
       },
       {
         name: "Next.js",
         image:
           "https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png",
-        years: 1,
+        years: today.getFullYear() - 2021,
       },
       {
         name: "Gatsby",
         image:
           "https://seeklogo.com/images/G/gatsby-logo-1A245AD37F-seeklogo.com.png",
-        years: 1,
+        years: today.getFullYear() - 2021,
+      },
+      {
+        name: "JestJS",
+        image: "https://jestjs.io/img/jest.png",
+        years: today.getFullYear() - 2021,
+      },
+      {
+        name: "React Testing Library",
+        image: "https://testing-library.com/img/octopus-128x128.png",
+        years: today.getFullYear() - 2021,
       },
       {
         name: "Emotion",
         image:
           "https://raw.githubusercontent.com/emotion-js/emotion/main/emotion.png",
-        years: 1,
-      },
-      {
-        name: "Styled Components",
-        image: "https://styled-components.com/atom.png",
-        years: 1,
+        years: today.getFullYear() - 2022,
       },
       {
         name: "Tailwind CSS",
         image:
           "https://tailwindcss.com/_next/static/media/tailwindcss-mark.79614a5f61617ba49a0891494521226b.svg",
-        years: 1,
+        years: today.getFullYear() - 2021,
+      },
+      {
+        name: "Java",
+        image:
+          "https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png",
+        years: today.getFullYear() - 2019,
+      },
+      {
+        name: "Python",
+        image: "https://img.icons8.com/color/48/000000/python--v1.png",
+        years: today.getFullYear() - 2021,
+      },
+      {
+        name: "Django",
+        image: "https://img.icons8.com/color/48/000000/django.png",
+        years: today.getFullYear() - 2021,
+      },
+      {
+        name: "Vue",
+        image: "https://img.icons8.com/color/48/000000/vue-js.png",
+        years: today.getFullYear() - 2021,
       },
     ],
   },
@@ -72,42 +105,47 @@ const tech = [
       {
         name: "Node.js",
         image: "https://img.icons8.com/color/48/000000/nodejs.png",
-        years: 2,
+        years: today.getFullYear() - 2020,
       },
       {
         name: "Express",
         image: "https://img.icons8.com/color/48/000000/express.png",
-        years: 2,
+        years: today.getFullYear() - 2020,
       },
       {
         name: "MongoDB",
         image: "https://img.icons8.com/color/48/000000/mongodb.png",
-        years: 2,
+        years: today.getFullYear() - 2020,
       },
       {
         name: "MySQL",
         image: "https://img.icons8.com/color/48/000000/mysql-logo.png",
-        years: 3,
+        years: today.getFullYear() - 2019,
+      },
+      {
+        name: "SQL",
+        image: "https://img.icons8.com/color/48/000000/sql.png",
+        years: today.getFullYear() - 2019,
       },
       {
         name: "GraphQL",
         image: "https://img.icons8.com/color/48/000000/graphql.png",
-        years: 1,
+        years: today.getFullYear() - 2021,
       },
       {
         name: "Apollo",
         image: "https://img.icons8.com/color/48/000000/apollo.png",
-        years: 1,
+        years: today.getFullYear() - 2021,
       },
       {
         name: "Firebase",
         image: "https://img.icons8.com/color/48/000000/firebase.png",
-        years: 1,
+        years: today.getFullYear() - 2021,
       },
       {
         name: "AWS",
         image: "https://img.icons8.com/color/48/000000/amazon-web-services.png",
-        years: 1,
+        years: today.getFullYear() - 2020,
       },
     ],
   },
