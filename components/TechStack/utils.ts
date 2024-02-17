1aa05464f1798774d041313dad99309f4775f768
Martín <martincalderon92@outlook.com>
2024-02-17 20:48:07 -0300

    Fix sorting of tech items by years

--- components/TechStack/utils.ts
+++ components/TechStack/utils.ts
@@ -2,5 +2,5 @@ import { techs } from './techs'
 
 export const sortedTechsByItemYears = techs?.map((tech) => ({
   ...tech,
-  items: tech.items.toSorted((a, b) => b.years - a.years)
+  items: [...tech.items].sort((a, b) => b.years - a.years)
 }))
