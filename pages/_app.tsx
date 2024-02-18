6402c35cd13a2ec8017f7cad38e238474116cea1
Martín <martincalderon92@outlook.com>
2024-02-17 23:12:06 -0300

    Add language setting to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react'
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    document.documentElement.lang = 'en' // Replace 'en' with the desired language code
+  }, [])
+
   return <Component {...pageProps} />
 }
 
