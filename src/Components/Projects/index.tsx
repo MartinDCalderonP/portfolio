8760e80030887630e4b97662d9cc164740048f03
Martín <martincalderon92@outlook.com>
2024-02-20 14:37:18 -0300

    Fix typos in project description

--- src/Components/Projects/index.tsx
+++ src/Components/Projects/index.tsx
@@ -21,7 +21,7 @@ const projects = [
   {
     title: 'Portfolio',
     description:
-      "This is the portofolio you're currently seeing. It's built with Next.js, TypeScript and Emotion. It's also hosted on Vercel.",
+      "This is the portfolio you're currently seeing. It's built with Next.js, TypeScript and Emotion. It's also hosted on Vercel.",
     image: Portfolio,
     tags: ['Next.js', 'TypeScript', 'Emotion', 'Vercel'],
     repoUrl: 'https://github.com/MartinDCalderonP/portfolio',
@@ -30,7 +30,7 @@ const projects = [
   {
     title: 'IMDB App',
     description:
-      'A IMDB app that allows you to search for movies and tv shows. It uses the OMDB API to fetch the data.',
+      'A IMDB app that allows you to search for movies and tv shows. It uses the IMDB API to fetch the data.',
     image: IMDBApp,
     tags: [
       'React',
