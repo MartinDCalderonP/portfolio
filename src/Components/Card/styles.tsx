10cafed355cd4a5d649ff4ac1031c07eb77e6867
Martín <martincalderon92@outlook.com>
2024-03-19 20:28:42 -0300

    Refactor Card component styles

--- src/Components/Card/styles.tsx
+++ src/Components/Card/styles.tsx
@@ -2,19 +2,13 @@ import styled from '@emotion/styled'
 import { motion } from 'framer-motion'
 
 export const Container = styled(motion.div)`
+  align-items: center;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #f5ebe0;
-  min-height: 80vh;
   height: fit-content;
-  width: 90%;
-  border: 0.063rem solid #dba39a;
-  border-radius: 2rem;
-  box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.75);
-  padding: 1rem;
-  margin: 2rem 1rem 1rem;
+  justify-content: center;
+  min-height: 80vh;
+  padding-top: 3.5rem;
 `
 
 export const TitleUnderline = styled.div`
