19709ca489c08a078b8380dc887098dfe025e667
Martín <martincalderon92@outlook.com>
2024-03-19 20:28:36 -0300

    Refactor Navbar styles

--- src/Components/Navbar/styles.tsx
+++ src/Components/Navbar/styles.tsx
@@ -2,23 +2,26 @@ import styled from '@emotion/styled'
 import { motion } from 'framer-motion'
 
 export const Container = styled.nav`
-  position: sticky;
-  top: 0;
-  left: 0;
-  width: 100%;
+  align-items: center;
+  background-color: #f5ebe0;
+  box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: flex-end;
-  align-items: center;
+  left: 0;
+  min-height: 4.375rem;
   padding: 0.5rem 2rem;
-  box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.5);
-  background-color: #f5ebe0;
+  position: sticky;
+  top: 0;
+  width: 100%;
   z-index: 100;
 `
 
 export const List = styled.ul`
+  align-items: center;
   display: flex;
-  list-style: none;
   gap: 1rem;
+  justify-content: center;
+  list-style: none;
 `
 
 export const ListItem = styled(motion.li)`
