346eb48eabd9f83b51767c965fadb0e980dc0219
Martín <martincalderon92@outlook.com>
2024-03-19 20:13:14 -0300

    Change Years to Year in Techs objects array

--- src/Components/TechStack/techs.ts
+++ src/Components/TechStack/techs.ts
@@ -1,11 +1,11 @@
 const today = new Date()
 const currentYear = today.getFullYear()
-const sinceTwentyEighteen = currentYear - 2018
-const sinceTwentyNineteen = currentYear - 2019
-const sinceTwentyTwenty = currentYear - 2020
-const sinceTwentyTwentyOne = currentYear - 2021
-const sinceTwentyTwentyTwo = currentYear - 2022
-const sinceTwentyTwentyThree = currentYear - 2023
+const TwentyEighteen = currentYear - 2018
+const TwentyNineteen = currentYear - 2019
+const TwentyTwenty = currentYear - 2020
+const TwentyTwentyOne = currentYear - 2021
+const TwentyTwentyTwo = currentYear - 2022
+const TwentyTwentyThree = currentYear - 2023
 
 export const techs = [
   {
@@ -14,177 +14,177 @@ export const techs = [
       {
         name: 'HTML 5',
         image: 'https://img.icons8.com/color/48/000000/html-5--v1.png',
-        years: sinceTwentyNineteen
+        year: TwentyNineteen
       },
       {
         name: 'CSS 3',
         image: 'https://img.icons8.com/color/48/000000/css3.png',
-        years: sinceTwentyNineteen
+        year: TwentyNineteen
       },
       {
         name: 'SASS',
         image: 'https://img.icons8.com/color/48/000000/sass.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'JavaScript',
         image: 'https://img.icons8.com/color/48/000000/javascript--v1.png',
-        years: sinceTwentyNineteen
+        year: TwentyNineteen
       },
       {
         name: 'TypeScript',
         image: 'https://img.icons8.com/color/48/000000/typescript.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'React',
         image: 'https://img.icons8.com/color/48/000000/react-native.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'React Native',
         image: 'https://img.icons8.com/color/48/000000/react-native.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Next',
         image:
           'https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Gatsby',
         image:
           'https://seeklogo.com/images/G/gatsby-logo-1A245AD37F-seeklogo.com.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'jQuery',
         image:
           'https://seeklogo.com/images/J/jquery-logo-CFE6ECE363-seeklogo.com.png',
-        years: sinceTwentyNineteen
+        year: TwentyNineteen
       },
       {
         name: 'JestJS',
         image: 'https://jestjs.io/img/jest.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'React Testing Library',
         image: 'https://testing-library.com/img/octopus-128x128.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Emotion',
         image:
           'https://raw.githubusercontent.com/emotion-js/emotion/main/emotion.png',
-        years: sinceTwentyTwentyTwo
+        year: TwentyTwentyTwo
       },
       {
         name: 'Styled Components',
         image:
           'https://raw.githubusercontent.com/styled-components/brand/master/styled-components.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Tailwind CSS',
         image: 'https://img.icons8.com/color/48/000000/tailwindcss.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Bootstrap',
         image:
           'https://seeklogo.com/images/B/bootstrap-logo-3C30FB2A16-seeklogo.com.png',
-        years: sinceTwentyNineteen
+        year: TwentyNineteen
       },
       {
         name: 'Materialize',
         image:
           'https://seeklogo.com/images/M/materialize-logo-0FCAD8A6F8-seeklogo.com.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Material UI',
         image:
           'https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Chakra UI',
         image: 'https://img.icons8.com/color/48/000000/chakra-ui.png',
-        years: sinceTwentyTwentyTwo
+        year: TwentyTwentyTwo
       },
       {
         name: 'Java',
         image:
           'https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png',
-        years: sinceTwentyEighteen
+        year: TwentyEighteen
       },
       {
         name: 'Python',
         image: 'https://img.icons8.com/color/48/000000/python--v1.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Django',
         image: 'https://img.icons8.com/color/48/000000/django.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Vue',
         image: 'https://img.icons8.com/color/48/000000/vue-js.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'TanStack Query',
         image:
           'https://seeklogo.com/images/R/react-query-logo-1340EA4CE9-seeklogo.com.png',
-        years: sinceTwentyTwentyTwo
+        year: TwentyTwentyTwo
       },
       {
         name: 'Apollo Client',
         image:
           'https://seeklogo.com/images/A/apollo-logo-DC7DD3C444-seeklogo.com.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Vite',
         image:
           'https://seeklogo.com/images/V/vite-logo-BFD4283991-seeklogo.com.png',
-        years: sinceTwentyTwentyTwo
+        year: TwentyTwentyTwo
       },
       {
         name: 'Vitest',
         image:
           'https://seeklogo.com/images/V/vitest-logo-9ADDA575A5-seeklogo.com.png',
-        years: sinceTwentyTwentyTwo
+        year: TwentyTwentyTwo
       },
       {
         name: 'Redux',
         image: 'https://img.icons8.com/color/48/000000/redux.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'NPM',
         image: 'https://img.icons8.com/color/48/000000/npm.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Yarn',
         image:
           'https://seeklogo.com/images/Y/yarn-logo-F5E7A65FA2-seeklogo.com.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'PNPM',
         image:
           'https://seeklogo.com/images/P/pnpm-logo-21DC828CD8-seeklogo.com.png',
-        years: sinceTwentyTwentyThree
+        year: TwentyTwentyThree
       },
       {
         name: 'Drupal',
         image: 'https://img.icons8.com/color/48/000000/drupal.png',
-        years: sinceTwentyTwentyThree
+        year: TwentyTwentyThree
       }
     ]
   },
@@ -194,133 +194,133 @@ export const techs = [
       {
         name: 'Node.js',
         image: 'https://img.icons8.com/color/48/000000/nodejs.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Express',
         image: 'https://img.icons8.com/color/48/000000/express.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'MongoDB',
         image: 'https://img.icons8.com/color/48/000000/mongodb.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Mongoose',
         image: 'https://img.icons8.com/color/48/000000/mongoose.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'MySQL',
         image: 'https://img.icons8.com/color/48/000000/mysql-logo.png',
-        years: sinceTwentyEighteen
+        year: TwentyEighteen
       },
       {
         name: 'SQL',
         image: 'https://img.icons8.com/color/48/000000/sql.png',
-        years: sinceTwentyEighteen
+        year: TwentyEighteen
       },
       {
         name: 'GraphQL',
         image: 'https://img.icons8.com/color/48/000000/graphql.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Apollo',
         image: 'https://img.icons8.com/color/48/000000/apollo.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Firebase',
         image: 'https://img.icons8.com/color/48/000000/firebase.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'AWS',
         image: 'https://img.icons8.com/color/48/000000/amazon-web-services.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Heroku',
         image: 'https://img.icons8.com/color/48/000000/heroku.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Netlify',
         image:
           'https://cdn.iconscout.com/icon/free/png-512/netlify-3445756-2878767.png?f=avif&w=256',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Vercel',
         image:
           'https://seeklogo.com/images/V/vercel-logo-F748E39008-seeklogo.com.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Postman',
         image:
           'https://seeklogo.com/images/P/postman-logo-0087CA0D15-seeklogo.com.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'MariaDB',
         image:
           'https://seeklogo.com/images/M/mariadb-logo-3CC78F4035-seeklogo.com.png',
-        years: sinceTwentyTwenty
+        year: TwentyTwenty
       },
       {
         name: 'Git',
         image: 'https://img.icons8.com/color/48/000000/git.png',
-        years: sinceTwentyNineteen
+        year: TwentyNineteen
       },
       {
         name: 'GitHub',
         image: 'https://img.icons8.com/color/48/000000/github--v1.png',
-        years: sinceTwentyNineteen
+        year: TwentyNineteen
       },
       {
         name: 'GitLab',
         image: 'https://img.icons8.com/color/48/000000/gitlab.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'BitBucket',
         image: 'https://img.icons8.com/color/48/000000/bitbucket.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Jira',
         image: 'https://img.icons8.com/color/48/000000/jira.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Confluence',
         image: 'https://img.icons8.com/color/48/000000/confluence.png',
-        years: sinceTwentyTwentyOne
+        year: TwentyTwentyOne
       },
       {
         name: 'Docker',
         image: 'https://img.icons8.com/color/48/000000/docker.png',
-        years: sinceTwentyTwentyTwo
+        year: TwentyTwentyTwo
       },
       {
         name: 'Nest',
         image: 'https://img.icons8.com/color/48/000000/nestjs.png',
-        years: sinceTwentyTwentyThree
+        year: TwentyTwentyThree
       },
       {
         name: 'Prisma',
         image:
           'https://seeklogo.com/images/P/prisma-logo-3805665B69-seeklogo.com.png',
-        years: sinceTwentyTwentyThree
+        year: TwentyTwentyThree
       },
       {
         name: 'Swagger',
         image:
           'https://seeklogo.com/images/S/swagger-logo-A49F73BAF4-seeklogo.com.png',
-        years: sinceTwentyTwentyThree
+        year: TwentyTwentyThree
       }
     ]
   }
